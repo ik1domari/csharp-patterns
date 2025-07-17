3e52a542cdf5a66debc858273e0497e3ae8181b4
Timur <astominsflavadept@gmail.com>
2025-07-17 19:39:49 +0500

    Command pattern

---
+++ Command/Commands/InsertTextCommand.cs
@@ -0,0 +1,29 @@
+﻿using Command.Entities;
+using Command.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Commands
+{
+    internal class InsertTextCommand : ICommand
+    {
+        TextEditor editor;
+        string text;
+        public InsertTextCommand(TextEditor editor, string text) {
+            this.editor = editor;
+            this.text = text;
+        }
+        public void Execute()
+        {
+            editor.InsertText(text);
+        }
+
+        public void Undo()
+        {
+            
+        }
+    }
+}
---
+++ Command/Entities/TextEditor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Entities
+{
+    internal class TextEditor
+    {
+        private string text;
+
+        public void InsertText(string text)
+        {
+            this.text = text;
+        }
+
+        public void DeleteLastCharacter() { 
+            this.text = this.text.Remove(this.text.Length - 1);
+        }
+
+        public string GetText()
+        {
+            return this.text;
+        }
+    }
+}
